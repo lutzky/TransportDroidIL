b173336068f174ad7f96ea5ded170025ef03f82b
Ohad Lutzky <ohad@lutzky.net>
2012-08-17 21:07:55 +0300

    Show current provider name in preferences

--- TransportDroidIL/src/net/lutzky/transportdroidil/Preferences.java
+++ TransportDroidIL/src/net/lutzky/transportdroidil/Preferences.java
@@ -1,9 +1,17 @@
 package net.lutzky.transportdroidil;
 
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
-public class Preferences extends PreferenceActivity {
+public class Preferences extends PreferenceActivity implements
+		OnSharedPreferenceChangeListener {
+	private ListPreference providerPreference;
+	private SharedPreferences sharedPreferences;
+
 	/*
 	 * Preference activities are deprecated in favor of preference fragments,
 	 * but those are only supported from API 11. (non-Javadoc)
@@ -14,7 +22,61 @@ public class Preferences extends PreferenceActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
+		sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
 		addPreferencesFromResource(R.xml.preferences);
+	}
+
+	@SuppressWarnings("deprecation")
+	ListPreference getProviderPreference() {
+		if (providerPreference == null) {
+			providerPreference = (ListPreference) findPreference("provider");
+		}
+
+		return providerPreference;
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		updateProviderSummary();
+		sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+	}
+
+	private void updateProviderSummary() {
+		String currentProviderKey = sharedPreferences.getString("provider", "");
+		if (currentProviderKey.length() == 0) {
+			getProviderPreference().setSummary("");
+			return;
+		}
+		getProviderPreference().setSummary(
+				getProviderName(sharedPreferences.getString("provider", "")));
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
+	}
+
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+		updateProviderSummary();
+	}
+
+	private String getProviderName(String providerKey) {
+		String provider_names[] = getResources().getStringArray(
+				R.array.providers_names);
+		String provider_values[] = getResources().getStringArray(
+				R.array.provider_values);
+
+		for (int i = 0; i < provider_values.length; ++i) {
+			if (provider_values[i].equals(providerKey)) {
+				return provider_names[i];
+			}
+		}
+
+		return "";
 	}
 }
